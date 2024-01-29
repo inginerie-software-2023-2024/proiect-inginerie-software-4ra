dba5c25efd165cff2e4bd1fe74a5571f43590969
ruxiii <ruxandra.iftimi@gmail.com>
2024-01-29 16:48:30 +0200

    Final project (I think)

--- Traveling-Platform/Traveling-Platform/Controllers/BookingsController.cs
+++ Traveling-Platform/Traveling-Platform/Controllers/BookingsController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Traveling_Platform.Data;
 using Traveling_Platform.Models;
 using System.Dynamic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Traveling_Platform.Controllers
 {
@@ -34,24 +35,26 @@ namespace Traveling_Platform.Controllers
         }
 
         // GET: Bookings
+        [Authorize(Roles = "Admin,HotelManager,User")]
         public IActionResult Index()
         {
             if (User.IsInRole("Admin"))
             {
                 var adminbookings = from book in db.Bookings
-                               select new
-                               {
-                                   Id = book.Id,
-                                   bookdate = book.BookingDate,
-                                   checkin = book.Checkin,
-                                   checkout = book.Checkout,
-                                   email = (from usr in db.Users where usr.Id == book.IdUser select usr.Email).First(),
-                                   hotel = (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.name).First(),
-                                   city = (from cit in db.Cities where (cit.Id == 
-                                           (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.id_city).First())
-                                           select cit.Name).First(),
-                                   room = (from rom in db.Rooms where rom.Id == book.IdRoom select rom.Name).First()
-                               };
+                                    select new
+                                    {
+                                        Id = book.Id,
+                                        bookdate = book.BookingDate,
+                                        checkin = book.Checkin,
+                                        checkout = book.Checkout,
+                                        email = (from usr in db.Users where usr.Id == book.IdUser select usr.Email).First(),
+                                        hotel = (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.name).First(),
+                                        city = (from cit in db.Cities
+                                                where (cit.Id ==
+                                                (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.id_city).First())
+                                                select cit.Name).First(),
+                                        room = (from rom in db.Rooms where rom.Id == book.IdRoom select rom.Name).First()
+                                    };
 
                 var lista = adminbookings.ToList();
 
@@ -60,63 +63,144 @@ namespace Traveling_Platform.Controllers
                 return View();
             }
 
-            
-            var bookings = db.Bookings.Where(b => b.IdUser == _userManager.GetUserId(User));
-            ViewBag.Lista = bookings;
+
+            if (User.IsInRole("HotelManager"))
+            {
+                var adminbookings = from book in db.Bookings.Where(b => (db.Hotels.Where(h => b.IdHotel == h.id_hotel).First().id_manager) == _userManager.GetUserId(User))
+                                    select new
+                                    {
+                                        Id = book.Id,
+                                        bookdate = book.BookingDate,
+                                        checkin = book.Checkin,
+                                        checkout = book.Checkout,
+                                        email = (from usr in db.Users where usr.Id == book.IdUser select usr.Email).First(),
+                                        hotel = (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.name).First(),
+                                        city = (from cit in db.Cities
+                                                where (cit.Id ==
+                                                (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.id_city).First())
+                                                select cit.Name).First(),
+                                        room = (from rom in db.Rooms where rom.Id == book.IdRoom select rom.Name).First()
+                                    };
+
+                var lista = adminbookings.ToList();
+
+                ViewBag.Lista = lista;
+
+                return View();
+            }
+
+            if (User.IsInRole("User"))
+            {
+                var adminbookings = from book in db.Bookings.Where(b => b.IdUser == _userManager.GetUserId(User))
+                                    select new
+                                    {
+                                        Id = book.Id,
+                                        bookdate = book.BookingDate,
+                                        checkin = book.Checkin,
+                                        checkout = book.Checkout,
+                                        email = (from usr in db.Users where usr.Id == book.IdUser select usr.Email).First(),
+                                        hotel = (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.name).First(),
+                                        city = (from cit in db.Cities
+                                                where (cit.Id ==
+                                                (from hot in db.Hotels where hot.id_hotel == book.IdHotel select hot.id_city).First())
+                                                select cit.Name).First(),
+                                        room = (from rom in db.Rooms where rom.Id == book.IdRoom select rom.Name).First()
+                                    };
+
+                var lista = adminbookings.ToList();
+
+                ViewBag.Lista = lista;
+
+                return View();
+            }
+
             return View();
         }
+
+        /*
+         public IActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return View(db.Reviews.ToList());
+            }
+
+            var hotel = db.Hotels.Find(id);
+            ViewBag.Nume = hotel.name;
+
+            var reviews = new List<ReviewViewModel>();
+            foreach (Review rev in db.Reviews.Where(r => r.IdHotel == id).ToList())
+            {
+                ReviewViewModel review = new ReviewViewModel();
+                review.Id = rev.Id;
+                review.Time = rev.Time;
+                review.Text = rev.Text;
+                review.ClientName = db.Users.Find(rev.IdClient).FirstName + " " + db.Users.Find(rev.IdClient).LastName;
+                review.HotelName = db.Hotels.Find(rev.IdHotel).name;
+                reviews.Add(review);
+            }
+
+            ViewBag.revs = reviews;
+            return View(); // Pass the 'reviews' list to the view
+        }
+         */
 
         // GET: Bookings/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || db.Bookings == null)
-            {
-                return NotFound();
-            }
+            Booking book = db.Bookings.Find(id);
+            Booking b = new Booking();
+            b.Id = book.Id;
+            b.BookingDate = book.BookingDate;
+            b.Checkin = book.Checkin;
+            b.Checkout = book.Checkout;
+            b.IdUser = book.IdUser;
+            b.Hotel = db.Hotels.Find(book.IdHotel);
+            b.Room = db.Rooms.Find(book.IdRoom);
 
-            var booking = await db.Bookings
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
+            return View(b);
         }
+
+        /* if (id == null || db.Bookings == null)
+         {
+             return NotFound();
+         }
+
+         var booking = await db.Bookings
+             .FirstOrDefaultAsync(m => m.Id == id);
+         if (booking == null)
+         {
+             return NotFound();
+         }
+
+         return View(booking);
+     */
 
         // GET: Bookings/Create
-        public IActionResult Create(int id)
+        public IActionResult Create()
         {
+            int hotidul = (int)TempData["hotid"];
             Booking book = new Booking();
-            int hotid = id;
-            ViewBag.hotid = hotid;
-            return RedirectToAction("Done");
-            book.Rooms = GetAllRooms(hotid);
-            TempData["hotid"] = hotid;
-            return View();
+            book.Rooms = GetAllRooms(hotidul);
+            ViewBag.UserId = _userManager.GetUserId(User);
+            ViewBag.HotelId = hotidul;
+            return View(book);
         }
-
-        public IActionResult Done()
-        {
-            return View();
-        }
-
 
         // POST: Bookings/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Booking book)
+        public async Task<IActionResult> Create([Bind("Id,BookingDate,Checkin,Checkout,IdUser,IdHotel,IdRoom")] Booking book)
         {
-            
-            book.IdHotel = (int)TempData["hotid"];
-            book.IdUser = _userManager.GetUserId(User);
-            book.BookingDate = DateTime.Now;
-            
-            db.Add(book);
-            await db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            {
+                db.Add(book);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
         }
 
 
@@ -203,21 +287,21 @@ namespace Traveling_Platform.Controllers
             {
                 db.Bookings.Remove(booking);
             }
-            
+
             await db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool BookingExists(int id)
         {
-          return (db.Bookings?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (db.Bookings?.Any(e => e.Id == id)).GetValueOrDefault();
         }
 
         public IEnumerable<SelectListItem> GetAllRooms(int id)
         {
             var selectList = new List<SelectListItem>();
             var rooms = from cat in db.Rooms.Where(r => r.IdHotel == id)
-                         select cat;
+                        select cat;
             foreach (var room in rooms)
             {
                 selectList.Add(new SelectListItem
